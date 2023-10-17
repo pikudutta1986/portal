d9909535048a73f46548475812e0e312fbadb04e
Anindya Dutta <dutta.ani007@gmail.com>
2023-10-17 10:06:59 +0530

    Update environment.prod.ts

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  apiBase: 'http://206.189.231.209/server/index.php/api/',
-  s3AccessKeyId: 'AKIA56MWS254XLU64PO7',
-  s3SecretAccessKey: 'OBY5zvYBXYmlv2750lxsdD5wmT//wbYEi9IP9Nv5',
-  s3BucketName: 'tuco-portal',
-  s3Region: 'us-east-1'
+  apiBase: 'add your api base url here',
+  s3AccessKeyId: 'add your AWS S3 access key ID here',
+  s3SecretAccessKey: 'add your AWS S3 secret key here',
+  s3BucketName: 'add your AWS S3 bucket here',
+  s3Region: 'add your AWS S3 bucket region here'
 };
